83b3c7baba1daa86658a5044e872ca32dcc63dc3
candini <candini@5cd43b9e-05fb-48e7-b25c-9272b6fb9088>
2012-11-06 09:47:35 +0000

    Added inputCreationDescription.txt

---
+++ software/3D-CMCC-Input/script/createInputForSE.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Description:	Creates input images for Forest Scenarios Evolution project (ForSE)
+# Author:		Alessandro Candini
+# Contact:		candini@meeo.it
+# Date:			2012-11-06
+# Version:		0.1
+#
+
