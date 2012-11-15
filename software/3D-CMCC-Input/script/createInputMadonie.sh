8c9dde17835a9e2eb1e38ddd695d44b9084a8b06
candini <candini@5cd43b9e-05fb-48e7-b25c-9272b6fb9088>
2012-11-15 13:56:01 +0000

    Up.

--- software/3D-CMCC-Input/script/createInputMadonie.sh
+++ software/3D-CMCC-Input/script/createInputMadonie.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 ### File information summary  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - {
 # Description:    createInputForSE.sh bash shell script
 #                 Processing chain to create input images for Forest Scenarios Evolution project (ForSE).
@@ -15,12 +15,56 @@
 VERSION="0.1"
 SCRIPT_NAME="${0:2:-3}"
 AOI="Parco delle Madonie (Sicily)"
-BIN_DIR="$( dirname ${0} )/../bin"
-WORK_DIR="$( dirname ${0} )/../output/working"
 MODULES=(remap getLAI applyMask)
 IMG_ALL=(Filters Y_planted Species Phenology Management NumHa AvDBH Height Wf Wrc Ws SolarRad Avg_Temp VPD Precip LAI)
 IMG_SELECTED=()
 
+BIN_DIR="$( dirname ${0} )/../bin"
+OUTPUT_DIR="$( dirname ${0} )/../output"
+# Input directories
+IN_00="$( dirname ${0} )/../input/00_Filters"
+IN_01="$( dirname ${0} )/../input/01_Y_planted"
+IN_02="$( dirname ${0} )/../input/02_Species"
+IN_03="$( dirname ${0} )/../input/03_Phenology"
+IN_04="$( dirname ${0} )/../input/04_Management"
+IN_05="$( dirname ${0} )/../input/05_NumHa"
+IN_06="$( dirname ${0} )/../input/06_AvDBH"
+IN_07="$( dirname ${0} )/../input/07_Height"
+IN_08="$( dirname ${0} )/../input/08_Wf"
+IN_09="$( dirname ${0} )/../input/09_Wrc"
+IN_10="$( dirname ${0} )/../input/10_Ws"
+IN_11="$( dirname ${0} )/../input/11_SolarRad"
+IN_12="$( dirname ${0} )/../input/12_Avg_Temp"
+IN_13="$( dirname ${0} )/../input/13_VPD"
+IN_14="$( dirname ${0} )/../input/14_Precip"
+IN_15="$( dirname ${0} )/../input/15_LAI"
+# Output directories
+OUT_00="$( dirname ${0} )/../output/00_Filters"
+OUT_01="$( dirname ${0} )/../output/01_Y_planted"
+OUT_02="$( dirname ${0} )/../output/02_Species"
+OUT_03="$( dirname ${0} )/../output/03_Phenology"
+OUT_04="$( dirname ${0} )/../output/04_Management"
+OUT_05="$( dirname ${0} )/../output/05_NumHa"
+OUT_06="$( dirname ${0} )/../output/06_AvDBH"
+OUT_07="$( dirname ${0} )/../output/07_Height"
+OUT_08="$( dirname ${0} )/../output/08_Wf"
+OUT_09="$( dirname ${0} )/../output/09_Wrc"
+OUT_10="$( dirname ${0} )/../output/10_Ws"
+OUT_11="$( dirname ${0} )/../output/11_SolarRad"
+OUT_12="$( dirname ${0} )/../output/12_Avg_Temp"
+OUT_13="$( dirname ${0} )/../output/13_VPD"
+OUT_14="$( dirname ${0} )/../output/14_Precip"
+OUT_15="$( dirname ${0} )/../output/15_LAI"
+
+# Output geotiff size:
+IMG_SIZE="1286x1160"
+# Output geotiff resolution:
+RES="30"
+# Output geotiff Upper Left point coordinates:
+IMG_UL="4211980.487859229557216 399333.291887304978445"
+# Output geotiff projection:
+PROJ="+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+PROJ_32="+proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 
 # DEBUG="n" --> clean the current working directory
 # DEBUG="y" --> do not clean the current working directory
@@ -43,6 +87,14 @@ usage(){
 log() {
 	echo -en "$(date +"%Y-%m-%d %H:%M:%S") - ${1}" | tee -a "${LOGFILE}"
 	#echo -en "$(date +"%Y-%m-%d %H:%M:%S") - ${1}" >> "${LOGFILE}"
+}
+
+check() {
+	if [ ${?} -ne "0" ] ; then
+		log "ERROR: ${1}.\n"
+	else
+		log "... done.\n"
+	fi
 }
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  Global functions definitions }
 
@@ -82,7 +134,7 @@ fi
 ### Pre-execution settings- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - {
 # Processing log filename with and without path
 LOGFILENAME="${SCRIPT_NAME}.log"
-LOGFILE="${WORK_DIR}/${LOGFILENAME}"
+LOGFILE="${OUTPUT_DIR}/${LOGFILENAME}"
 
 # Remove logfile if already existent
 if [ -f "${LOGFILE}" ] ; then
@@ -97,6 +149,69 @@ log "Starting data processing for ForSE project (version ${VERSION})\n"
 log "Area Of Interest: ${AOI}\n"
 log "\n"
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  Pre-execution settings }
+
+### Filters execution - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - {
+# Filters images are produced indepentently on script input parameters
+#log "### { Start creating Filters images...... ###\n"
+#PAR_01="-co COMPRESS=LZW -of GTiff"
+#MSG="Conversion of AOI shapefile into tiff"
+#INPUT_01="${IN_00}/Sicily.shp"
+#OUTPUT_01="${WORK_DIR}/Sicily.tif"
+#log "${MSG} ...\n"
+#gdal_rasterize ${PAR_01} -burn 255 -tr 0.00045266691956530711 0.00045266691956530711 -ot Byte ${INPUT_01} ${OUTPUT_01} &>> "${LOGFILE}"
+#check "${MSG} failed on ${INPUT_01}.\n"
+
+#MSG="Conversion of tiff projection from longlat to UTM"
+#OUTPUT_02="${WORK_DIR}/Madonie.tif"
+#log "${MSG} ...\n"
+#gdalwarp ${PAR_01} -t_srs "${PROJ}" -tr ${RES} -${RES} ${OUTPUT_01} ${OUTPUT_02} &>> "${LOGFILE}"
+#check "${MSG} failed on ${OUTPUT_01}.\n"
+
+#MSG="Remap of UTM geotiff image"
+#OUTPUT_03="${WORK_DIR}/Madonie_remap.tif"
+#log "${MSG} ...\n"
+#${BIN_DIR}/remap -i ${OUTPUT_02} -o ${OUTPUT_03} -s ${RES} -m -l ${IMG_UL} -e ${IMG_SIZE} -w 5x5 &>> "${LOGFILE}"
+#check "${MSG} failed on ${OUTPUT_02}.\n"
+
+#for INPUT_02 in $( ls ${IN_00}/*.asc ) ; do
+	#	MSG="Conversion from ASCII corine format into geotiff of ${INPUT_02}"
+	#	OUTPUT_04="${WORK_DIR}/$( basename $( echo ${INPUT_02} | sed s/.asc/.tif/ ) )"
+	#	log "${MSG} ...\n"
+	#	gdal_translate ${PAR_01} -a_srs "${PROJ_32}" ${INPUT_02} ${OUTPUT_04} &>> "${LOGFILE}"
+	#	check "${MSG} failed on ${INPUT_02}.\n"
+	
+	#	MSG="Changing zone from 32 to 33 of ${OUTPUT_04}"
+	#	OUTPUT_05="${WORK_DIR}/$( basename $( echo ${INPUT_02} | sed s/.asc/_zone33.tif/ ) )"
+	#	log "${MSG} ...\n"
+	#	gdalwarp ${PAR_01} -t_srs "${PROJ}" ${OUTPUT_04} ${OUTPUT_05} &>> "${LOGFILE}"
+	#	check "${MSG} failed on ${OUTPUT_04}.\n"
+
+	#	MSG="Remap of UTM geotiff image"
+	#	OUTPUT_06="${WORK_DIR}/$( basename $( echo ${INPUT_02} | sed s/.asc/_Madonie_30m.tif/ ) )"
+	#	log "${MSG} ...\n"	
+	#	${BIN_DIR}/remap -i ${OUTPUT_05} -o ${OUTPUT_06} -s ${RES} -m -l ${IMG_UL} -e ${IMG_SIZE} -w 5x5 &>> "${LOGFILE}"
+	#	check "${MSG} failed on ${OUTPUT_05}.\n"
+#done
+
+	
+	#cp ${OUT_MSK}/${SCENE_ID}-${PROD_DATE}-LAI_crop.tif ${FINAL_OUTPUT}
+	#if [ ${?} -ne 0 ] ; then
+		#	log "Copy of LAI cropped image into output dir failed.\n"
+		#	error "Copy of LAI cropped image into output dir failed."
+	#fi
+	
+	# Clean input/working and packet dir
+	#if [ ${DEBUG} == "n" ] ; then
+		#	rm -r ${WORK_DIR}/*
+		#	if [ ${?} -ne "0" ] ; then
+			#		echo "Cleaning "${WORK_DIR}" failed\n"
+			#	exit 4
+		#fi
+	#fi
+
+
+log "### .......stop creating Filters images } ###\n"
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Filters execution }
 
 ### Y_planted execution - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - {
 for IMG in "${IMG_SELECTED[@]}" ; do
@@ -228,10 +343,6 @@ done
 for IMG in "${IMG_SELECTED[@]}" ; do
 	if [ "${IMG}" == "LAI" ] ; then
     	log "### { Start creating ${IMG} images.......... ###\n"
-    	IMAGES=(
-    	)
-	
-    	${BIN_DIR}/getLAI -i ${OUT_NDV}/${SCENE_ID}-${PROD_DATE}-NDVI.tif -o ${OUT_LAI}/${SCENE_ID}-${PROD_DATE}-LAI.tif &>> "${LOGFILE}"
     	log "### ...........stop creating ${IMG} images } ###\n"
     fi
 done
