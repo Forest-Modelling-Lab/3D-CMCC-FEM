f2aa7646f41d82ed14ac11dd0bdaf4ca59afb902
collalti <collalti@5cd43b9e-05fb-48e7-b25c-9272b6fb9088>
2012-11-28 10:05:28 +0000

    up

--- software/3D-CMCC-Forest-Model/src/soilmodel.c
+++ software/3D-CMCC-Forest-Model/src/soilmodel.c
@@ -21,23 +21,16 @@ void soil_model(CELL *c, const YOS *const yos, const int years, const int month,
 	Log("--SOIL MODEL ROUTINE--\n");
 
 	Log("*********GET MONTHLY SOIL WATER BALACE************\n");
-	/*Take off Evapotranspiration*/
-	c->available_soil_water -= c->evapotranspiration ;
 
-	Log("ASW at the END of month less Evapotraspiration = %g mm\n", c->available_soil_water);
-	if ( c->available_soil_water < 0)
-	{
-		Log("ATTENTION Available Soil Water is low than MinASW!!! \n");
-		c->available_soil_water = site->minAsw;
-		Log("ASW = %g\n", c->available_soil_water);
-	}
-	if ( c->available_soil_water > site->maxAsw)
-	{
-		Log("ATTENTION Available Soil Water exceeds MAXASW!! \n");
-		c->available_soil_water = site->maxAsw;
-		Log("Available soil water = %g\n", c->available_soil_water);
-	}
+	//from BIOME
+	/* convert kg/m2 --> m3/m2 --> m3/m3 */
+	/*
+	vwc = soilw / (1000.0 * sitec->soil_depth);
+	*/
+	/* calculate psi */
+	/*
+	*psi = sitec->psi_sat * pow((vwc/sitec->vwc_sat), sitec->soil_b);
+	*psi */0.
 
-	/* ok */
-	//return 1;
+
 }
