c28ebb0cdd21a075648a423d10f58b12bed10aa1
candini <candini@5cd43b9e-05fb-48e7-b25c-9272b6fb9088>
2012-12-12 13:47:34 +0000

    Up.

--- software/3D-CMCC-Spatial/script/applySpecieMask.sh
+++ software/3D-CMCC-Spatial/script/applySpecieMask.sh
@@ -1,35 +1,71 @@
 #!/bin/bash
 
-# Species identification numbers: 
-# 0 = "-9999" (Undefined)
-# 1 = "Castaneasativa"
-# 2 = "Fagussylvatica"
-# 3 = "Ostryacarpinifolia" 
-# 4 = "Pinusnigra"
-# 5 = "Quercuscerris"
-# 6 = "quercus_deciduous" (Q. cerris, Q. robur, Q. pubescens, Q. petreae)
-# 7 = "quercus_evergreen" (Q. ilex, Q. suber)
-SPECIES_ID=(Undefined Castaneasativa Fagussylvatica Ostryacarpinifolia Pinusnigra Quercuscerris quercus_deciduous quercus_evergreen)
-SPECIES_ID_PRESENT=(1 2 6 7)
-PREF="Madonie"
 YEARS=(2002 2003 2004 2005 2006 2007 2008 2009)
+MONTHS=(1 2 3 4 5 6 7 8 9 10 11 12)
 
-for IDX in "${SPECIES_ID_PRESENT[@]}" ; do
-	SPECIES_MASK_NAMES[${IDX}]="${PREF}_${SPECIES_ID[${IDX}]}_${IDX}_mask.tif"
-  	SPECIES_MASK[${IDX}]="${SPECIES_MASK_NAMES[${IDX}]}"		
+MASK="Titerno-Tammaro_quercus_evergreen_7_mask.tif"
+EMPTY="empty_good.tif"
+PAR="-q -co COMPRESS=LZW -of GTiff -ot Float32 -separate"
+
+IMG5BANDS=(AvDBH Height Y_planted Management N_cell Phenology Species)
+IMG12BANDS=(SolarRad Avg_Temp VPD Precip LAI)
+
+gdal_translate -b 2 ${IMG5BANDS[0]}.tif ${EMPTY}
+
+for S in ${IMG5BANDS[@]} ; do
+	gdal_calc.py -A ${S}.tif -B ${MASK} --outfile=${S}_new.tif --calc="(A*B)"
+	gdal_merge.py ${PAR} ${S}_new.tif ${EMPTY} ${EMPTY} ${EMPTY} ${EMPTY} -o ${S}_good.tif
+	rm ${S}_new.tif
 done
 
-for X in GPP NPP ; do
+for C in ${IMG12BANDS[@]} ; do
 	for Y in ${YEARS[@]} ; do
-		for Z in ${SPECIES_MASK[@]} ; do
-			IMG="${X}_sum_${Y}.tif"
-			MSK="${Z}"
-			POSTFIX="${MSK:${#PREF}}"
-			OUT_TMP="${X}_sum_${Y}${POSTFIX}"
-			OUT=$( echo ${OUT_TMP} | sed s/_mask// )
-			gdal_calc.py -A ${IMG} -B ${MSK} --outfile=${OUT} --calc="(A*B)"
+		BANDS=()
+		for M in ${MONTHS[@]} ; do
+			IN="${C}_${Y}.tif"
+			OUT="${C}_${Y}_b${M}.tif"			
+			gdal_calc.py -A ${IN} --A_band=${M} -B ${MASK} --outfile=${OUT} --calc="(A*B)"
+			BANDS+=("${OUT}")
 		done
+		gdal_merge.py ${PAR} ${BANDS[@]} -o ${C}_${Y}_good.tif
+		rm ${BANDS[@]}
 	done
+done
+
+BANDS=()
+for B in 1 2 3 4 5 ; do
+	IN="Soil.tif"
+	OUT="Soil_b${B}.tif"			
+	gdal_calc.py -A ${IN} --A_band=${B} -B ${MASK} --outfile=${OUT} --calc="(A*B)"
+	BANDS+=("${OUT}")
+done
+gdal_merge.py ${PAR} ${BANDS[@]} -o Soil_good.tif
+rm ${BANDS[@]}
+
+rm ${EMPTY}
+
+OLD=()
+NEW=()
+for S in ${IMG5BANDS[@]} ; do
+	OLD+=("${S}.tif")
+	NEW+=("${S}_good.tif")
+done
+
+for S in ${IMG12BANDS[@]} ; do
+	for Y in ${YEARS[@]} ; do
+		OLD+=("${S}_${Y}.tif")
+		NEW+=("${S}_${Y}_good.tif")
+	done
+done
+
+OLD+=("Soil.tif")
+NEW+=("Soil_good.tif")
+
+rm ${OLD[@]}
+
+for N in ${NEW[@]} ; do
+	#echo "mv ${N} $( echo ${N} | sed s/_good// )"
+	mv ${N} $( echo ${N} | sed s/_good// )
 done
 
 exit 0
