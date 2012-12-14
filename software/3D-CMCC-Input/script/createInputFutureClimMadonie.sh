75b34d9a1295e33abdc24db709cddb666c541087
candini <candini@5cd43b9e-05fb-48e7-b25c-9272b6fb9088>
2012-12-14 13:23:08 +0000

    Up.

--- software/3D-CMCC-Input/script/createInputFutureClimMadonie.sh
+++ software/3D-CMCC-Input/script/createInputFutureClimMadonie.sh
@@ -997,7 +997,7 @@ for IMG in "${IMG_SELECTED[@]}" ; do
 			Y_COORD=$( echo ${P} | cut -d ',' -f '2' )
 			NEW_X_COORD=$( echo "(${X_COORD}-1)+0.5" | bc | sed 's/^\./0./' )
 			NEW_Y_COORD=$( echo "((${SIZEY_EUROPE}-1)-(${Y_COORD}-1))+0.5" | bc | sed 's/^\./0./' )
-			LINE_NUM=$( cat ${TEMPLATE_TXT} | grep -wn "[ \t]*${NEW_X_COORD}[ \t]*${NEW_Y_COORD}" | cut -d ':' -f '1' )
+			#LINE_NUM=$( cat ${TEMPLATE_TXT} | grep -wn "[ \t]*${NEW_X_COORD}[ \t]*${NEW_Y_COORD}" | cut -d ':' -f '1' )
 
 			for YYYY in "${YEARS_PROC[@]}" ; do
 				for MM in "${MONTHS_PROC[@]}" ; do
@@ -1005,7 +1005,8 @@ for IMG in "${IMG_SELECTED[@]}" ; do
 					INPUT_02="${WK_13}/Avg_Temp_${YYYY}${MM}.txt"
 					INPUT_03="${WK_13}/Temp_Range_${YYYY}${MM}.txt"
 					
-					TE=( $( sed -n -e "${IDX},${IDX}p" ${INPUT_01} ) )
+					#AVG_TEMP=$( grep -n '${NEW_X_COORD} ${NEW_Y_COORD}' ${INPUT_02} )
+					#echo ${AVG_TEMP}
 				
 					#ORIG_VAL="${YEAR_DATA[${K}]}"
 					#if [ "${ORIG_VAL}" == "${MISSING}" ] ; then
