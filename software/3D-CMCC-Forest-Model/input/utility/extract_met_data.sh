d2aefea37625bb04c09406d02ed602fe975a73c2
collalti <collalti@5cd43b9e-05fb-48e7-b25c-9272b6fb9088>
2013-04-10 09:58:52 +0000

    add extract_met_data.sh and utility folder

---
+++ software/3D-CMCC-Forest-Model/input/utility/extract_met_data.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+#create folder of downloaded data
+# mkdir downloaded_files
+
+#TO DO scarica i file dal sito ucea
+# wget http://old.politicheagricole.it/ucea/forniture/index3.htm
+
+cd CRA_MET_DATA
+
+filesToParse=($(find -type f -name '*.gz'))
+
+
+echo "choose a site and write (in capital letters)"
+read  \site 
+echo "site choiced :" "$site"
+
+#control file
+if [[ -a EXTRACTED_"$site".txt ]] ; then
+  echo 'file EXTRACTED_'"$site"'.txt still exist '
+  echo 'delete old  and create a new EXTRACTED_'"$site"'.txt '
+  rm  EXTRACTED_"$site".txt
+else
+  echo 'file doesnt exist'
+  echo 'create  EXTRACTED_'"$site"'.txt '
+fi
+
+for I in ${filesToParse[@]} ; do
+	gunzip -dc ${I} | grep  "$site" >> EXTRACTED_"$site".txt
+done
+
+
+
+
+#control row
+if [[ -s EXTRACTED_"$site".txt ]] ; then
+  echo 'file EXTRACTED_'"$site"'.txt is valid'
+  else
+  echo 'file EXTRACTED_'"$site"'.txt is empty'
+fi
+
+
+
+mv  EXTRACTED_"$site".txt .. 
+
+
+exit 0
