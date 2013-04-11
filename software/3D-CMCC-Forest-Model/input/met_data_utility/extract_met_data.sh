816856a414a60445b4638f51c0c2157e147e5822
candini <candini@5cd43b9e-05fb-48e7-b25c-9272b6fb9088>
2013-04-11 08:23:42 +0000

    Updated extract_met_data.sh: now produces per year files

--- software/3D-CMCC-Forest-Model/input/met_data_utility/extract_met_data.sh
+++ software/3D-CMCC-Forest-Model/input/met_data_utility/extract_met_data.sh
@@ -8,12 +8,9 @@
 
 cd CRA_MET_DATA
 
-
-
-
 echo "choose a site and write (in capital letters)"
 read  \site 
-echo "site choiced :" "$site"
+echo "site choiced : ${site}"
 
 echo "chose a variable between: TMAX, TMIN, RAD, PREC, UMI"
 read \variable
@@ -25,46 +22,43 @@ if  [[ "${variable}" == "TMAX" ]] ||
     [[ "${variable}" == "UMI" ]] ; then
     cd "${variable}"
 else
-  echo ''"$variable"' is a bad choice!!!'
-  exit 0
+    echo "${variable} is a bad choice!!!"
+    exit 0
 fi
-
-
 
 filesToParse=($(find -type f -name '*.gz'))
 
-#control file
-if [[ -a EXTRACTED_"$site".txt ]] ; then
-  echo 'file EXTRACTED_'"$site"'.txt still exist '
-  echo 'delete old  OR create a new EXTRACTED_'"$site"'.txt '
-  rm  EXTRACTED_"$site_$variable".txt
-else
-  echo 'file doesnt exist'
-  echo 'create  EXTRACTED_'"$site"_"$variable"'.txt '
-fi
-
 for I in ${filesToParse[@]} ; do
-	gunzip -dc ${I} | grep  "$site" >> EXTRACTED_"$site"_"$variable".txt
+    yearTmp=$(echo "${I}" | cut -d _ -f 5)
+    year="${yearTmp:0:4}"
+    fileName=EXTRACTED_"${site}"_"${variable}"_"${year}".txt
+
+    #control file
+    if [[ -a "${fileName}" ]] ; then
+        echo "file ${fileName} still exist"
+        echo "delete old  OR create a new ${fileName}"
+        rm  ${fileName}
+    else
+        echo "file doesnt exist"
+        echo "create${fileName}"
+    fi
+
+    gunzip -dc ${I} | grep  "${site}" >> "${fileName}" 
+
+    #control files
+    if [[ -s "${fileName}" ]] ; then
+        echo "file ${fileName} is valid"
+    else
+        echo "file ${fileName} is empty"
+    fi
+
+    #control if files contain NO DATA
+    echo 'control no data values...'
+    cat "${fileName}" | sed 's/--/-9999/g' > "${fileName}".tmp
+    mv "${fileName}".tmp "${fileName}"
+
+    mv  "${fileName}" .. 
 done
-
-
-
-
-#control files
-if [[ -s EXTRACTED_"$site"_"$variable".txt ]] ; then
-  echo 'file EXTRACTED_'"$site"_"$variable"'.txt is valid'
-  else
-  echo 'file EXTRACTED_'"$site"_"$variable"'.txt is empty'
-fi
-
-#control if files contain NO DATA
-echo 'control no data values...'
-cat EXTRACTED_"$site"_"$variable".txt | sed 's/--/-9999/g' > EXTRACTED_"$site"_"$variable".txt.tmp
-mv EXTRACTED_"$site"_"$variable".txt.tmp EXTRACTED_"$site"_"$variable".txt
-
-
-
-mv  EXTRACTED_"$site"_"$variable".txt .. 
 
 
 exit 0
