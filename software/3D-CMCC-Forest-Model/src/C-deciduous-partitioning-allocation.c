47a15dfc84b4bd5e8e5e8c75fc76acb633e93c5e
acollalti <alessio.collalti@cmcc.it>
2014-03-21 16:17:01 +0100

    hainich site and input data

--- software/3D-CMCC-Forest-Model/src/C-deciduous-partitioning-allocation.c
+++ software/3D-CMCC-Forest-Model/src/C-deciduous-partitioning-allocation.c
@@ -236,6 +236,13 @@ void D_Get_Partitioning_Allocation (SPECIES *const s, CELL *const c, const MET_D
 			if (s->value[BIOMASS_RESERVE] < 0.0)
 			{
 				Log("ATTENTION BIOMASS RESERVE < 0.0\n");
+				frac_to_foliage_fineroot = 0;
+				Log("fraction of reserve for foliage and fine root = %f\n", frac_to_foliage_fineroot);
+			}
+			else
+			{
+				frac_to_foliage_fineroot = (s->value[BIOMASS_RESERVE]) / s->counter[BUD_BURST_COUNTER];
+				Log("fraction of reserve for foliage and fine root = %f\n", frac_to_foliage_fineroot);
 			}
 			Log("++Remaining days for bud burst = %d\n", s->counter[BUD_BURST_COUNTER]);
 
@@ -247,14 +254,12 @@ void D_Get_Partitioning_Allocation (SPECIES *const s, CELL *const c, const MET_D
 			 * sharing the daily remaining amount (taking into account respiration costs)of NSC */
 			//Angelo try to change with a exponential function as frac_to_foliage = s->value[BIOMASS_RESERVE] * (e^-s->value[BUD_BURST])
 			//fixme try to allocate just a part of total reserve not all
-			frac_to_foliage_fineroot = (s->value[BIOMASS_RESERVE]) / s->counter[BUD_BURST_COUNTER];
-			Log("fraction of reserve for foliage and fine root = %f\n", frac_to_foliage_fineroot);
+
 
 			/*partitioning*/
 			if (s->value[NPP] > 0.0)
 			{
 				Log("Using reserve and npp\n");
-
 				s->value[DEL_FOLIAGE] = (frac_to_foliage_fineroot * (1.0 - s->value[FINE_ROOT_LEAF_FRAC]))	+ (s->value[NPP] * (1.0 - s->value[FINE_ROOT_LEAF_FRAC]));
 				s->value[DEL_ROOTS_FINE_CTEM] = (frac_to_foliage_fineroot * s->value[FINE_ROOT_LEAF_FRAC]) + (s->value[NPP] * s->value[FINE_ROOT_LEAF_FRAC]);;
 				s->value[DEL_RESERVE] = - frac_to_foliage_fineroot;
@@ -266,16 +271,29 @@ void D_Get_Partitioning_Allocation (SPECIES *const s, CELL *const c, const MET_D
 			}
 			else
 			{
-				Log("Using ONLY reserve\n");
-
-				s->value[DEL_FOLIAGE] = (frac_to_foliage_fineroot * (1.0 - s->value[FINE_ROOT_LEAF_FRAC]));
-				s->value[DEL_ROOTS_FINE_CTEM] = (frac_to_foliage_fineroot * s->value[FINE_ROOT_LEAF_FRAC]);
-				s->value[DEL_RESERVE] = ((s->value[C_FLUX] * GC_GDM)/1000000) * (s->value[CANOPY_COVER_DBHDC]* settings->sizeCell)- frac_to_foliage_fineroot;
-				s->value[DEL_ROOTS_COARSE_CTEM] = 0;
-				s->value[DEL_ROOTS_TOT] = 0;
-				s->value[DEL_TOT_STEM] = 0;
-				s->value[DEL_STEMS]= 0;
-				s->value[DEL_BB]= 0;
+				if (s->value[BIOMASS_RESERVE] > 0)
+				{
+					Log("Using ONLY reserve\n");
+					s->value[DEL_FOLIAGE] = (frac_to_foliage_fineroot * (1.0 - s->value[FINE_ROOT_LEAF_FRAC]));
+					s->value[DEL_ROOTS_FINE_CTEM] = (frac_to_foliage_fineroot * s->value[FINE_ROOT_LEAF_FRAC]);
+					s->value[DEL_RESERVE] = ((s->value[C_FLUX] * GC_GDM)/1000000) * (s->value[CANOPY_COVER_DBHDC]* settings->sizeCell)- frac_to_foliage_fineroot;
+					s->value[DEL_ROOTS_COARSE_CTEM] = 0;
+					s->value[DEL_ROOTS_TOT] = 0;
+					s->value[DEL_TOT_STEM] = 0;
+					s->value[DEL_STEMS]= 0;
+					s->value[DEL_BB]= 0;
+				}
+				else
+				{
+					Log("No reserve no NPP\n");
+					s->value[DEL_ROOTS_FINE_CTEM] = (frac_to_foliage_fineroot * s->value[FINE_ROOT_LEAF_FRAC]);
+					s->value[DEL_RESERVE] = ((s->value[C_FLUX] * GC_GDM)/1000000) * (s->value[CANOPY_COVER_DBHDC]* settings->sizeCell)- frac_to_foliage_fineroot;
+					s->value[DEL_ROOTS_COARSE_CTEM] = 0;
+					s->value[DEL_ROOTS_TOT] = 0;
+					s->value[DEL_TOT_STEM] = 0;
+					s->value[DEL_STEMS]= 0;
+					s->value[DEL_BB]= 0;
+				}
 			}
 
 			/*allocation*/
