57dc25efb6ebc147c02f432feddb797cc752890f
angelo <angelo.nole@gmail.com>
2014-06-19 18:21:44 +0200

    prova angelo

--- software/3D-CMCC-Forest-Model/src/allometry.c
+++ software/3D-CMCC-Forest-Model/src/allometry.c
@@ -20,8 +20,6 @@ void Get_allometry (SPECIES *const s, AGE *const a, int years)
 	//tree height from champan-richards function
 
 
-
-
 	if (!years)
 	{
 	s->value[INITIAL_TREE_HEIGHT_CR] = 1.3 + s->value[CRA] * pow (1 - exp ( - s->value[CRB] * s->value[AVDBH]) , s->value[CRC]);
