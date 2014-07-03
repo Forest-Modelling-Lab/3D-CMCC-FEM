23d2d0ea448f30397951f1c518cf22a8cd6f2132
acollalti <alessio.collalti@cmcc.it>
2014-07-03 15:18:46 +0200

    up

--- software/3D-CMCC-Forest-Model/src/C-deciduous-partitioning-allocation.c
+++ software/3D-CMCC-Forest-Model/src/C-deciduous-partitioning-allocation.c
@@ -360,7 +360,6 @@ void D_Get_Partitioning_Allocation (SPECIES *const s, CELL *const c, const MET_D
 				//s->value[LAI_SUN] = (s->value[BIOMASS_FOLIAGE] * 1000.0)*(s->value[LAI_RATIO]/(s->value[LAI_RATIO]+1.0));
 			}
 			/*for dominated shaded foliage*/
-			//fixme model wrongs multiplying SLA_RATIO
 			else
 			{
 				Log("computing LAI for dominated trees\n");
