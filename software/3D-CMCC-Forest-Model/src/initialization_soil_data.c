715e63c9adf26548b86812aeb5eff38e7654cfe0
alessio <alessio.collalti@cmcc.it>
2016-05-05 11:10:27 +0200

    stuffs

--- software/3D-CMCC-Forest-Model/src/initialization_soil_data.c
+++ software/3D-CMCC-Forest-Model/src/initialization_soil_data.c
@@ -18,12 +18,18 @@ void Initialization_site_data (CELL *c)
 	float bcoeff;
 	float sat;
 
-	Log("\n****SITE-SOIL CHARACTERISTICS for cell (%d, %d)****\n", c->x, c->y);
-	/*soil matric potential*/
+	float volumetric_wilting_point;
+	float volumetric_field_capacity;
+	float volumetric_saturated_hydraulic_conductivity;
 
+	Log("\n****SITE-SOIL CHARACTERISTICS for cell (%d, %d)****\n", c->x, c->y);
+	// (cm)   effective depth of rooting zone
+	Log("Soil depth = %f cm\n", site->soil_depth);
+
+	/*soil matric potential*/
 	CHECK_CONDITION(fabs((site->sand_perc + site->clay_perc + site->silt_perc) -100.0 ), > 1e-4);
 	
-	Log("Biome soil characteristics\n");
+	Log("BIOME soil characteristics\n");
 	//double soilw_fc; //maximum volume soil water content in m3/m3
 	// (DIM) Clapp-Hornberger "b" parameter
 	c->soil_b = -(3.10 + 0.157*site->clay_perc - 0.003*site->sand_perc);
@@ -38,13 +44,9 @@ void Initialization_site_data (CELL *c)
 	c->vwc_fc =  c->vwc_sat * pow((-0.015/c->psi_sat),(1.0/c->soil_b));
 	Log ("volumetric water content at field capacity (BIOME) = %f %(vol) \n", c->vwc_fc);
 
-
-	//todo definire quale determina il MAXASW
 	// define maximum soilwater content, for outflow calculation
 	//converts volumetric water content (m3/m3) --> (kg/m2)
-	// (cm)   effective depth of rooting zone
-	Log("Soil depth = %f cm\n", site->soil_depth);
-	//Log("Soil depth = %f m\n", site->soil_depth / 100);
+
 	// (kgH2O/m2) soil water at field capacity
 	c->soilw_fc = (site->soil_depth / 100) * c->vwc_fc * 1000.0;
 	Log ("soilw_fc BIOME (MAXASW FC BIOME)= %f (kgH2O/m2)\n", c->soilw_fc);
@@ -65,37 +67,46 @@ void Initialization_site_data (CELL *c)
 
 	/* soil data from https://www.nrel.colostate.edu/projects/century/soilCalculatorHelp.htm */
 	/* following Saxton et al 1986 */
-	Log("Century soil characteristics\n");
+	Log("CENTURY soil characteristics\n");
 	acoeff = exp(-4.396 - 0.0715 * site->clay_perc - 4.88e-4 * pow(site->sand_perc,2) - 4.285e-5 * pow(site->sand_perc,2)*site->clay_perc);
 	bcoeff = (-3.14 - 0.00222 * pow(site->clay_perc,2) - 3.484e-5 * pow(site->sand_perc,2) * site->clay_perc);
 	sat = (0.332 - 7.251e-4 * site->sand_perc + 0.1276 * log10(site->clay_perc));
 
 	/* volumetric percentage for wilting point */
-	c->wilting_point = pow((15.0/acoeff), (1.0/bcoeff));
+	volumetric_wilting_point = pow((15.0/acoeff), (1.0/bcoeff));
 	/* volumetric percentage field capacity */
-	c->field_capacity = pow((0.333/acoeff),(1.0/bcoeff));
+	volumetric_field_capacity = pow((0.333/acoeff),(1.0/bcoeff));
 	/* volumetric percentage saturated hydraulic conductivity */
-	c->sat_hydr_conduct = exp((12.012 - 0.0755 * site->sand_perc) + (-3.895 + 0.03671 * site->sand_perc - 0.1103 * site->clay_perc + 8.7546e-4 * pow(site->clay_perc,2))/sat);
+	volumetric_saturated_hydraulic_conductivity = exp((12.012 - 0.0755 * site->sand_perc) + (-3.895 + 0.03671 * site->sand_perc - 0.1103 * site->clay_perc + 8.7546e-4 * pow(site->clay_perc,2))/sat);
 	/* bulk density g/cm3 */
 	c->bulk_density = (1 - sat) * 2.65;
 
 	/* corrections from Steve Del Grosso */
 	/* volumetric percentage wilting point */
-	c->wilting_point += (-0.15 * c->wilting_point);
-	Log("volumetric water content at wilting point (CENTURY) = %f %(vol)\n", c->wilting_point);
+	volumetric_wilting_point += (-0.15 * volumetric_wilting_point);
+	Log("*volumetric water content at wilting point (CENTURY) = %f %(vol)\n", volumetric_wilting_point);
+	/* (kgH2O/m2) soil water at wilting point */
+	c->wilting_point = (site->soil_depth / 100) * volumetric_wilting_point * 1000.0;
+	Log("**Wilting point (CENTURY) = %f mm/m2\n", c->wilting_point);
 
 	/* volumetric percentage field capacity */
-	c->field_capacity += (0.07 * c->field_capacity);
-	Log("volumetric water content at field capacity (CENTURY) = %f %(vol)\n", c->field_capacity);
+	volumetric_field_capacity += (0.07 * volumetric_field_capacity);
+	Log("*volumetric water content at field capacity (CENTURY) = %f %(vol)\n", volumetric_field_capacity);
+	/* (kgH2O/m2) soil water at field capacity */
+	c->field_capacity = (site->soil_depth / 100) * volumetric_field_capacity * 1000.0;
+	Log("**Field capacity (CENTURY) = %f mm/m2\n", c->field_capacity);
 
 	/* volumetric percentage saturated hydraulic conductivity */
-	c->sat_hydr_conduct /= 1500.0;
-	Log("volumetric water content at saturated hydraulic conductance (CENTURY) = %f %(vol)\n", c->sat_hydr_conduct);
+	volumetric_saturated_hydraulic_conductivity /= 1500.0;
+	Log("*volumetric water content at saturated hydraulic conductance (CENTURY) = %f %(vol)\n", volumetric_saturated_hydraulic_conductivity);
+	//fixme not clear what it is
+	/* (kgH2O/m2) soil water at saturated hydraulic conductivity */
+	c->sat_hydr_conduct = (site->soil_depth / 100) * volumetric_saturated_hydraulic_conductivity * 1000.0;
+	Log("**Saturated hydraulic conductivity (CENTURY) = %f mm/m2\n", c->sat_hydr_conduct);
 
 	/* bulk density g/cm3 */
 	c->bulk_density += (-0.08 * c->bulk_density);
-	Log("bulk density = %f g/cm^3\n", c->bulk_density);
+	Log("**Bulk density = %f g/cm^3\n", c->bulk_density);
 
-	exit(1);
 }
 
--- software/3D-CMCC-Forest-Model/src/modifiers.c
+++ software/3D-CMCC-Forest-Model/src/modifiers.c
@@ -188,8 +188,8 @@ void Daily_modifiers (SPECIES *const s, AGE *const a, CELL *const c, const MET_D
 	c->soil_moist_ratio = c->asw/c->max_asw;
 	s->value[F_SW] = 1.0 / (1.0 + pow(((1.0 - c->soil_moist_ratio) / s->value[SWCONST]), s->value[SWPOWER]));
 	CHECK_CONDITION(s->value[F_SW], > 1.0);
-	Log("ASW = %f\n", c->asw);
-	Log("MIN ASW = %f\n", c->max_asw * settings->init_frac_maxasw);
+	Log("ASW = %f mm/m2\n", c->asw);
+	Log("Wilting point = %f mm/m2\n", c->wilting_point);
 	Log("moist ratio = %f\n", c->soil_moist_ratio);
 	Log("fSW = %f\n", s->value[F_SW]);
 
@@ -203,33 +203,39 @@ void Daily_modifiers (SPECIES *const s, AGE *const a, CELL *const c, const MET_D
 	/* Uses the multivariate regressions from Cosby et al., 1984 */
 	/* (DIM) volumetric water content */
 	c->vwc = c->asw / (1000.0 * (site->soil_depth/100));
-	Log("volumetric available soil water  = %f (DIM)\n", c->vwc);
+	Log("volumetric available soil water  = %f %(vol)\n", c->vwc);
 	Log ("vwc_sat = %f (DIM)\n", c->vwc_sat);
 	Log ("vwc/vwc_sat = %f \n", c->vwc / c->vwc_sat);
 	c->psi = c->psi_sat * pow((c->vwc/c->vwc_sat), c->soil_b);
 	Log ("PSI BIOME = %f (MPa)\n", c->psi);
 	Log ("PSI_SAT BIOME = %f (MPa)\n", c->psi_sat);
 
-	if (c->psi > s->value[SWPOPEN]) /*no water stress*/
+	/*no water stress*/
+	if (c->psi > s->value[SWPOPEN])
 	{
 		counter_water_stress = 0.0;
 		s->value[F_PSI] = 1.0;
 	}
-	else if (c->psi <= s->value[SWPCLOSE]) /* full water stress */
+	// 5 MAY 2016 ADDED "|| (c->asw < c->wilting_point)
+	/* full water stress */
+	else if (c->psi <= s->value[SWPCLOSE] /* && (c->asw < c->wilting_point)*/)
 	{
 		//change for multiple class
 		counter_water_stress += 1;
 		//s->value[F_PSI] = 0.0;
 		Log("Water stress\n");
 		Log("F_PSI = %f\n", s->value[F_PSI]);
+
 		/* forced to 0.33 to avoid too much low values */
 		//fixme
 		s->value[F_PSI] = 0.33;
 		Log("F_PSI = %f\n", s->value[F_PSI]);
 		//CHECK_CONDITION(counter_water_stress, > 31);
 	}
-	else /* partial water stress */
+	/* partial water stress */
+	else
 	{
+		counter_water_stress = 0.0;
 		s->value[F_PSI] = (s->value[SWPCLOSE] - c->psi)/(s->value[SWPCLOSE] - s->value[SWPOPEN]);
 	}
 	Log("F_PSI = %f\n", s->value[F_PSI]);
