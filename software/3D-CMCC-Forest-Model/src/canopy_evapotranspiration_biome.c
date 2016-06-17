c4d1733b4741be3c46fa29df1f8c2f68add74952
alessio-cmcc-laptop <alessio.collalti@cmcc.it>
2016-06-17 23:43:03 +0200

    new (to be tested) interception (for rain and snow) based on Lawrence et al., 2007

--- software/3D-CMCC-Forest-Model/src/canopy_evapotranspiration_biome.c
+++ software/3D-CMCC-Forest-Model/src/canopy_evapotranspiration_biome.c
@@ -85,6 +85,22 @@ void canopy_evapotranspiration_biome (SPECIES *const s, CELL *const c, const MET
 
 	daylength_sec = met[month].d[day].daylength * 3600.0;
 
+	//test following Lawrence et al., 2006
+	//todo change INT_COEFF to 0.25
+	/* for rain */
+	if(c->prcp_rain != 0.0)
+	{
+		s->value[CANOPY_INT] = s->value[INT_COEFF]*c->prcp_rain*(1.0 - exp(-0.5 * s->value[LAI]));
+		logger(g_log, "CANOPY_INT with rain (Lowrence) = %f\n", s->value[CANOPY_INT]);
+	}
+	/* for snow */
+	else
+	{
+		s->value[CANOPY_INT] = s->value[INT_COEFF]*c->prcp_snow*(1.0 - exp(-0.5 * s->value[LAI]));
+		logger(g_log, "CANOPY_INT with snow (Lowrence) = %f\n", s->value[CANOPY_INT]);
+	}
+
+	//test check why a so low values for int coeff (should be 0.30??)
 	max_int = s->value[INT_COEFF] * s->value[ALL_LAI];
 
 	logger(g_log, "ALL_LAI = %f mm/m^2/day\n", s->value[ALL_LAI]);
