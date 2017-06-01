0a7b86f00cc424fd86d4e7137e202c7709303510
gaetano <gpellicone@gmail.com>
2017-06-01 11:46:16 +0200

    update

--- software/Calib-3D-CMCC-FEM/Model3DCMCC/R/getResults.R
+++ software/Calib-3D-CMCC-FEM/Model3DCMCC/R/getResults.R
@@ -1,21 +1,42 @@
 #' @export
 
-getResults<-function(outDir,nyears,truePath=FALSE){
+getResults<-function(outDir){
+  #nmonths <- nyears*12
+  #ndays <- nyears*365
+  
+  #Sys.setlocale("LC_ALL","English")
+  
+  #ifelse (truePath==FALSE,
+  #yes = newDir <- file.path(outDir, paste0("output_5.3.1_",format(Sys.Date(),"%Y"),"_",toupper(format(Sys.Date(),"%B")),"_",format(Sys.Date(),"%d"))),
+  #no = newDir <- outDir
+  #)
+  errorMessage <- "Error in running 3D-CMCC-CNR-FEM. Please check the log file in the main directory."
+  if (length(list.files(outDir))==0 ) {
+    cat("\n")
+    cat(message(errorMessage))
+    results <- errorMessage} else {
+  annual <- list.files(outDir, pattern = "annual",recursive = TRUE, full.names = TRUE)
+  monthly <- list.files(outDir, pattern = "monthly",recursive = TRUE, full.names = TRUE)
+  daily <- list.files(outDir, pattern = "daily",recursive = TRUE, full.names = TRUE)
+  
+  if ((file.exists(annual) & "#error:" %in% readLines(annual)) | 
+      (file.exists(monthly) & "#error:" %in% readLines(monthly)) | 
+      (file.exists(daily) & "#error:" %in% readLines(daily))){
+    cat("\n")
+    cat(message(errorMessage))
+    results <- errorMessage} else {
   results<-list()
-  nmonths <- nyears*12
-  ndays <- nyears*365
+  if (file.exists(annual)) results[["annual"]]<-read.csv(annual,header=T,comment.char = "#")
+  if (file.exists(monthly)) results[["monthly"]]<-read.csv(monthly,header=T,comment.char = "#")
+  if (file.exists(daily)) results[["daily"]]<-read.csv(daily,header=T,comment.char = "#")
+    }
   
-  Sys.setlocale("LC_ALL","English")
-  
-  ifelse (truePath==FALSE,
-  yes = newDir <- file.path(outDir,paste0("output_5.3.1_",format(Sys.Date(),"%Y"),"_",toupper(format(Sys.Date(),"%B")),"_",format(Sys.Date(),"%d"))),
-  no = newDir <- outDir
-  )
-  if (dir.exists(file.path(newDir,"annual"))) results[["annual"]]<-read.csv(list.files(file.path(newDir,"annual"),full.names = T)[1],nrows=nyears,header=T)  
-  if (dir.exists(file.path(newDir,"monthly")))results[["monthly"]]<-read.csv(list.files(file.path(newDir,"monthly"),full.names = T)[1],nrows=nmonths,header=T)
-  if (dir.exists(file.path(newDir,"daily"))) results[["daily"]]<-read.csv(list.files(file.path(newDir,"daily"),full.names = T)[1],nrows=ndays,header=T)
-  
-  Sys.setlocale()
-  
+  #sys.setlocale()
+    }
   return(results)     
 }
+
+#annual <- list.files("D:/RR/Github/TG18/3D-CMCC/Simul_Model/sensitivity/ITbon_gpp/output/", pattern = "annual",recursive = TRUE, full.names = TRUE)
+#monthly <- list.files("D:/RR/Github/TG18/3D-CMCC/Simul_Model/sensitivity/ITbon_gpp/output/", pattern = "monthly",recursive = TRUE, full.names = TRUE)
+#daily <- list.files("D:/RR/Github/TG18/3D-CMCC/Simul_Model/sensitivity/ITbon_gpp/output/", pattern = "daily",recursive = TRUE, full.names = TRUE)
+
