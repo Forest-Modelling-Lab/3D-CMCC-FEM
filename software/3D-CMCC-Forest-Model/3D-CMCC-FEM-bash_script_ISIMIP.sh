750dc01445bcdc0565a1055c776b7cd5a2a8508d
alessio <alessio.collalti@cmcc.it>
2017-06-09 14:38:38 +0200

    added 2Bpico and 2BLBCpico meteo files

--- software/3D-CMCC-Forest-Model/3D-CMCC-FEM-bash_script_ISIMIP.sh
+++ software/3D-CMCC-Forest-Model/3D-CMCC-FEM-bash_script_ISIMIP.sh
@@ -738,24 +738,14 @@ for (( e = 0 ; e < $exp_counter ; ++e )) ; do
 	#FT_runs
 	
 	#3  simulation for '2A' experiment
-	2A_runs
+	#2A_runs
 	
 	#3  simulation for '2B' experiment
 	#2B_runs
 	
 	#4  simulation for '2BLBC' experiment
-	#2BLBC_runs
+	2BLBC_runs
 done
-
-
-#3  simulation for '2A' experiment
-#2A_runs
-#
-#3  simulation for '2B' experiment
-#2B_runs
-#
-#2  simulation for '2B' experiment
-#2BLBC_runs
 
 #########################################################################################################
 
