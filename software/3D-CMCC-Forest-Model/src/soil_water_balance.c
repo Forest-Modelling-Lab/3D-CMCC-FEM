212a06858829f870e126702360847983a215f588
corrado <corrado@corrado-Pavilion>
2017-10-30 13:58:55 +0100

    up

--- software/3D-CMCC-Forest-Model/src/soil_water_balance.c
+++ software/3D-CMCC-Forest-Model/src/soil_water_balance.c
@@ -65,7 +65,7 @@ void soil_water_balance(cell_t *c, const meteo_daily_t *const meteo_daily)
 	}
 
 	/* soil moist ratio (%vol.) */
-	//test
+	//test computing soil moist ratio or get it from met data (to possibly avoid)
 //	if ( -9999 == meteo_daily->swc )
 //	{
 		c->soil_moist_ratio = c->asw / c->max_asw_fc;
