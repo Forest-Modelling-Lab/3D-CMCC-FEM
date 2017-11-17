f5ae02523674af5c389eaf0a2f8f7d77530979f6
alessio <alessio.collalti@cmcc.it>
2017-11-17 15:28:12 +0100

    added header files

---
+++ software/3D-CMCC-Forest-Model/headers/litter_nitrogen_balance.h
@@ -0,0 +1,13 @@
+/*
+ * litter_nitrogen_balance.h
+ *
+ *  Created on: 17 nov 2017
+ *      Author: alessio
+ */
+
+#ifndef HEADERS_LITTER_NITROGEN_BALANCE_H_
+#define HEADERS_LITTER_NITROGEN_BALANCE_H_
+
+void litter_nitrogen_balance (cell_t *const c);
+
+#endif /* HEADERS_LITTER_NITROGEN_BALANCE_H_ */
