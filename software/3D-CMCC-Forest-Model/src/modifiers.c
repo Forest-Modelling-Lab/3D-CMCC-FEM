d150db583ed0c1ba9e8e234d9256a7a80335a39c
gimar15 <gina.marano@unina.it>
2020-09-30 14:52:55 +0200

    daniela's bug-fix psi

--- software/3D-CMCC-Forest-Model/src/modifiers.c
+++ software/3D-CMCC-Forest-Model/src/modifiers.c
@@ -450,7 +450,9 @@ void modifiers(cell_t *const c, const int layer, const int height, const int dbh
 	/* Uses the multivariate regressions from Cosby et al., 1984 */
 	/* volumetric water content */
 
+	//test Daniela bug-fix psi September 2020
 	c->vwc = c->asw / c->max_asw_fc;
+	c-> vwc= c-> asw/((c->soil_depth/100.)*1000.);
 	c->psi = c->psi_sat * pow((c->vwc/c->vwc_sat), c->soil_b);
 
 	/* no water stress */
