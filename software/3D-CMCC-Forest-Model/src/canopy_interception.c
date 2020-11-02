a8f5cb0e4678fbc87147d4dc02a46afae41835ff
ddalmo <daniela.dalmonech@gmail.com>
2020-11-02 09:54:00 +0100

    cancell comments in canopy_interception.c

--- software/3D-CMCC-Forest-Model/src/canopy_interception.c
+++ software/3D-CMCC-Forest-Model/src/canopy_interception.c
@@ -54,14 +54,6 @@ void canopy_interception(cell_t *const c, const int layer, const int height, con
 	/*************************************************************************/
 	/* shared functions among all class/layers */
 
-//ddalmo correction 13-2-2020  On some compilators LAI_PROJ!= 0 also when 
-// it should be ==0. The issue is related to LEAF_C at the end of the growing season, where the value should be==0, but there are precision issues..
-
-//if ( s->value[LAI_PROJ] < 0.000001 )
-// {s->value[LAI_PROJ]=0. ;
-// }
-
-
 	//note: not accumulation of rain or snow happens if canopy is wet or snowed */
 	if( ( meteo_daily->prcp > 0.) && ( s->value[LAI_PROJ] > 0. ) && ( ! s->value[CANOPY_WATER] ) && ( ! s->value[CANOPY_SNOW] ) )
 	{
