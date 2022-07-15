e2cca33cba123437cadcac372a73689c738b9d12
ddalmo <daniela.dalmonech@gmail.com>
2022-07-16 00:32:12 +0200

    FIX BUG on 5.6-Basilicata
    removed a // in tree_model.c  so to let the code perform the management routine.
    The line of code was accidentally commented in the phase of cleaning and commit.

--- software/3D-CMCC-Forest-Model/src/tree_model.c
+++ software/3D-CMCC-Forest-Model/src/tree_model.c
@@ -126,7 +126,7 @@ int Tree_model_daily (matrix_t *const m, const int cell, const int day, const in
                //if (MANAGEMENT_VAR == g_settings->management || MANAGEMENT_ON == g_settings->management)
                // {
 		/* management blocks */
-	     //	forest_management ( c, day, month, year ); 
+	     	forest_management ( c, day, month, year ); 
              // }  
  
 	}
